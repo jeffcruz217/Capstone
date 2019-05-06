6143c3ecba76f3c0dbe03522c48a98f038c915f8
jeffcruz217 <49079248+jeffcruz217@users.noreply.github.com>
2019-05-06 18:59:05 -0400

    Add files via upload

--- Help.py
+++ Help.py
@@ -1,5 +1,7 @@
 from firebase import firebase
-import schedule 
+import schedule
+import time
+
 firebase = firebase.FirebaseApplication('https://pillreminder2-c8807.firebaseio.com/',None)
 
 pillhour = firebase.get('/Current Schedule/Darel Diaz/Hours1',None)
