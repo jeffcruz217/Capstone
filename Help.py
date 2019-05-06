683f89aa042eeceba5af08d89bf859bcd834d185
jeffcruz217 <49079248+jeffcruz217@users.noreply.github.com>
2019-05-06 19:04:59 -0400

    Add files via upload

--- Help.py
+++ Help.py
@@ -3,14 +3,18 @@ import schedule
 import time
 
 firebase = firebase.FirebaseApplication('https://pillreminder2-c8807.firebaseio.com/',None)
+datarefresh = 0
+ 
+def Update:
 
-pillhour = firebase.get('/Current Schedule/Darel Diaz/Hours1',None)
+	pillhour = firebase.get('/Current Schedule/Darel Diaz/Hours1',None)
 
-pilltime2 = int(pillhour)
+	pilltime2 = int(pillhour)
 
 def Work():
 	print ("please")
 
+schedule.every(datarefresh).minutes.do(Update)
 schedule.every(pilltime2).minutes.do(Work)
 
 while True:
